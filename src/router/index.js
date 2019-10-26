c47ae125e4a7a70f3b5da85cea55b05fc40ee96c
kubo-hide-kun <hide44041@gmail.com>
2019-10-27 00:11:51 +0900

    Create markdown editor

--- src/router/index.js
+++ src/router/index.js
@@ -19,6 +19,11 @@ const routes = [
     path: '/signup',
     name: 'signup',
     component: () => import('../views/Signup.vue')
+  },
+  {
+    path: '/create',
+    name: 'create',
+    component: () => import('../views/EventCreateForm.vue')
   }
 ]
 
---
+++ src/views/EventCreateForm.vue
@@ -0,0 +1,26 @@
+<template>
+  <v-app>
+    <mavon-editor v-model="value" language="en" />
+  <button @click="print">ddd</button>
+  </v-app>
+</template>
+ 
+<script>
+import Vue from "vue";
+import mavonEditor from 'mavon-editor';
+import "mavon-editor/dist/css/index.css";
+Vue.use(mavonEditor);
+
+export default {
+  name: "app",
+  data() {
+    return { value: "" }; 
+  },
+  methods: {
+    print() {
+      console.log(this.value);  // eslint-disable-line
+      console.log(this);  // eslint-disable-line
+    }
+  }
+};
+</script> 
