2f51089d4b92e1349bb15cda9853b67eeae5cfd6
國益良太 <kunimasuryota@kunimasumasuryoutanoMacBook-Pro.local>
2019-10-26 22:49:36 +0900

    データの送信先の追加とデザインの修正

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,20 +1,42 @@
 <template>
-    <div class="signin">
-        <h1>ログイン</h1>
-        <p>アカウントをお持ちでない方はこちらから<router-link to="signup">新規アカウント登録</router-link>をお願いします</p>
-        <form name="signin" action="" method="get">
-            <div class="inputform">
-                <p class="idText">学籍番号</p>
-                <input type="text" class="textForm" name="number" size="22" maxlength="20" required>
-                <p class="passText">パスワード</p>
-                <input type="text" class="textForm" name="password" size="22" maxlength="20" required>
-                <p><input type="submit" class="buttonForm" name="loginButton" value="ログインする"></p>
-            </div>
-        </form>
+    <div class="cp_bgpattern20">
+        <div class="signin">
+            <h1>ログイン</h1>
+            <p>アカウントをお持ちでない方はこちらから<router-link to="signup">新規アカウント登録</router-link>をお願いします</p>
+            <form name="signin" action="https://hackfesfuk-api.azurewebsites.net/api/login" method="get">
+                <div class="inputform">
+                    <p class="idText">学籍番号</p>
+                    <input type="text" class="textForm" name="number" size="22" maxlength="20" required>
+                    <p class="passText">パスワード</p>
+                    <input type="text" class="textForm" name="password" size="22" maxlength="20" required>
+                    <p><input type="submit" class="buttonForm" name="loginButton" value="ログインする"></p>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
 <style>
+
+ /* 背景  */
+.cp_bgpattern20{
+	background-color: rgba(250,250,250,1);
+	background-image:radial-gradient(closest-side,transparent 99.9%, rgba(236,236,236,0.8) 100%),
+    radial-gradient(closest-side,transparent 99.9%, rgba(236,236,236,0.8) 100%);
+    background-position: 0 0, 30px 30px;
+	background-size: 60px 60px;
+    background-repeat: repeat;
+    bottom:0;
+    height:100%;
+}
+
+h1 {
+  background: rgb(50,120,200); /*背景色*/
+  padding: 0 0.5em;/*文字周りの余白*/
+  color: rgb(230,230,230);/*文字を白に*/
+  border-radius: 0.5em;/*角の丸み*/
+}
+
 .inputform{
     position: absolute;
     left: 50%;
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,23 +1,45 @@
 <template>
-    <div class="signup">
-        <h1>新規アカウント登録</h1>
-        <p>以下の項目を全て入力して「新規登録」ボタンをクリックしてください。<br>
-        すでにアカウント登録をされている方は<router-link to="signin">こちらからログイン</router-link>できます。</p>
-        <form name="signup" action="" method="get">
-            <div class="signupform">
-                <p class="nameText">名前</p>
-                <input type="text" class="textForm" name="name" size="22" maxlength="20" required>
-                <p class="idText">学籍番号</p>
-                <input type="text" class="textForm" name="number" size="22" maxlength="20" required>
-                <p class="passText">パスワード</p>
-                <input type="text" class="textForm" name="password" size="22" maxlength="20" required>
-                <p><input type="submit" class="buttonForm" name="signupButton" value="新規登録"></p>
-            </div>
-        </form>
+    <div class="cp_bgpattern20">
+        <div class="signup">
+            <h1>新規アカウント登録</h1>
+                <p>以下の項目を全て入力して「新規登録」ボタンをクリックしてください。<br>
+                すでにアカウント登録をされている方は<router-link to="signin">こちらからログイン</router-link>できます。</p>
+                <form name="signup" action="https://hackfesfuk-api.azurewebsites.net/api/create-user" method="get">
+                    <div class="signupform">
+                        <p class="nameText">名前</p>
+                        <input type="text" class="textForm" name="name" size="22" maxlength="20" required>
+                        <p class="idText">学籍番号</p>
+                        <input type="text" class="textForm" name="number" size="22" maxlength="20" required>
+                        <p class="passText">パスワード</p>
+                        <input type="text" class="textForm" name="password" size="22" maxlength="20" required>
+                        <p><input type="submit" class="buttonForm" name="signupButton" value="新規登録"></p>
+                    </div>
+                </form>
+        </div>
     </div>
 </template>
 
 <style>
+
+ /* 背景  */
+.cp_bgpattern20{
+	background-color: rgba(250,250,250,1);
+	background-image:radial-gradient(closest-side,transparent 99.9%, rgba(236,236,236,0.8) 100%),
+    radial-gradient(closest-side,transparent 99.9%, rgba(236,236,236,0.8) 100%);
+    background-position: 0 0, 30px 30px;
+	background-size: 60px 60px;
+    background-repeat: repeat;
+    bottom:0;
+    height:100%;
+}
+
+h1 {
+  background: rgb(50,120,200); /*背景色*/
+  padding: 0 0.5em;/*文字周りの余白*/
+  color: rgb(230,230,230);/*文字を白に*/
+  border-radius: 0.5em;/*角の丸み*/
+}
+
 .signupform{
     position: absolute;
     left: 50%;
