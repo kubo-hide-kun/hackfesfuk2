a25d0c9f6ea70f27edaf94876be7857bf5659307
kubo-hide-kun <hide44041@gmail.com>
2019-10-26 20:40:33 +0900

    Add support for routing

--- src/App.vue
+++ src/App.vue
@@ -3,20 +3,18 @@
     <SignedHeader v-if="this.$store.state.isSigned"/>
     <DefaultHeader v-else/>
     <v-content>
-      <Home/>
+      <router-view/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import Home from './views/Home';
 import DefaultHeader from './components/DefaultHeader';
 import SignedHeader from './components/SignedHeader';
 
 export default {
   name: 'App',
   components: {
-    Home,
     DefaultHeader,
     SignedHeader
   },
